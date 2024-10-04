4ee10ea4542ecd01692ed8a8848d0ac64a26c99a
biancaroman <biancaroman1425@gmail.com>
2024-10-03 23:22:18 -0300

    Adicionando configuracoes do banco e a classe Livro

--- data/MongoDbContext.cs
+++ data/MongoDbContext.cs
@@ -1,19 +1,2 @@
 ﻿using MongoDB.Driver;
-using Microsoft.Extensions.Configuration;
-using cp5_dotnet.model;
-
-namespace cp5_dotnet.data
-{
-    public class MongoDbContext
-    {
-        private readonly IMongoDatabase _database;
-
-        public MongoDbContext(IConfiguration configuration)
-        {
-            var client = new MongoClient(configuration.GetSection("MongoDbSettings:ConnectionString").Value);
-            _database = client.GetDatabase(configuration.GetSection("MongoDbSettings:DatabaseName").Value);
-        }
-
-        public IMongoCollection<Livro> Livros => _database.GetCollection<Livro>("Livros");
-    }
-}
+using Microsoft.Extension
