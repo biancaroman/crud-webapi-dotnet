183137bf0d269d57c9e856cab3a024d3cfd0f371
biancaroman <biancaroman1425@gmail.com>
2024-10-03 23:24:26 -0300

    Adicionando configuracoes do banco e a classe Livro

--- model/Livro.cs
+++ model/Livro.cs
@@ -1,2 +1,19 @@
 ﻿using MongoDB.Bson;
-using MongoDB.Bson.Serializa
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace cp5_dotnet.model
+{
+    public class Livro
+    {
+        [BsonId]  
+        [BsonRepresentation(BsonType.ObjectId)] 
+        public ObjectId Id { get; set; }
+
+        public string PublicId { get; set; } = Guid.NewGuid().ToString(); 
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnoPublicacao { get; set; }
+        public string Genero { get; set; }
+    }
+}
