b32c0780ebfc0183fc56530b09a76bfa0bb9cffc
biancaroman <biancaroman1425@gmail.com>
2024-10-03 23:17:44 -0300

    Adicionando configuracoes do banco e a classe Livro

--- model/obj/Debug/net8.0/model.AssemblyInfo.cs
+++ model/obj/Debug/net8.0/model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d08010fe02ecd51f1863ba0eeeea566977c0cb21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18756abb9cd272803eb0e758ea33b0e1525c5133")]
 [assembly: System.Reflection.AssemblyProductAttribute("model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
